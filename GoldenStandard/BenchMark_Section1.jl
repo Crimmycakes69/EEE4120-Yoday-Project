fd9d2119479e0ce23bd122a2a829b8f360a306a1
Christal Sima <88949990+ChristalSima@users.noreply.github.com>
2022-05-23 20:13:21 +0200

    Add files via upload
    Updated golden standard code

--- GoldenStandard/BenchMark_Section1.jl
+++ GoldenStandard/BenchMark_Section1.jl
@@ -1,11 +1,27 @@
-using WAV, TickTock, Plots
+#using WAV, TickTock, Plots
 
 #Read audio file into array
-array, fs = wavread("GuassianNoise5s.wav", format = "float")
+#array, fs = wavread("GuassianNoise5s.wav", format = "float")
 
-h = Plots.histogram(array, xlabel = "number generated", ylabel = "frequency", title = "random number distribution")
-Plots.display(h)
-readline()
+#h = Plots.histogram(array, xlabel = "number generated", ylabel = "frequency", title = "random number distribution")
+#Plots.display(h)
+#readline()
+
+using CSV, DataFrames, TickTock, Plots
+
+CSVFile = CSV.read("SineWave1s.csv",DataFrame)
+freq = 48000
+duration = 1
+audSize = freq*duration
+array = zeros(Int64, audSize)
+for i in 1:audSize
+    array[i] = convert(Int64, CSVFile[i,1])
+end
+#p = plot(1:audSize,array)
+#Plots.display(p)
+#readline()
+println("Number of samples: ",length(array))
+
 tick()
 Len = length(array)
 arrMax = array[1]
@@ -19,5 +35,5 @@ for i in 1:Len
     end
 end
 t = tock()
-println(arrMin)
-println(arrMax)
+println("Minimum: ",arrMin)
+println("Maximum: ",arrMax)
--- GoldenStandard/BenchMark_Section2.jl
+++ GoldenStandard/BenchMark_Section2.jl
@@ -1,12 +1,28 @@
-using WAV, TickTock, Plots
-N = 1 #Insert interval length here in seconds
+#using WAV, TickTock, Plots
 
 #Read audio file into array
-array, fs = wavread("GuassianNoise5s.wav", format = "float")
+#array, fs = wavread("GuassianNoise5s.wav", format = "float")
 
 #h = Plots.histogram(array, xlabel = "number generated", ylabel = "frequency", title = "random number distribution")
 #Plots.display(h)
 #readline()
+using CSV, DataFrames, TickTock, Plots
+
+CSVFile = CSV.read("SineWave10s.csv",DataFrame)
+freq = 48000
+duration = 10
+audSize = freq*duration
+array = zeros(Int64, audSize)
+for i in 1:audSize
+    array[i] = convert(Int64, CSVFile[i,1])
+end
+#p = plot(1:audSize,array)
+#Plots.display(p)
+#readline()
+println("Number of samples: ",length(array))
+
+fs = 48000
+N = 1 #Insert interval length here in seconds
 tick()
 Len = length(array)
 intervalNumber = convert(Int64,Len/(N*fs))
@@ -53,10 +69,10 @@ end
 
 t = tock()
 
-println(avgMax)
-println(STDDevMax)
-println(avgMin)
-println(STDDevMin)
-println(arrMax)
-println(arrMin)
+println("Minimum Average: ",avgMax)
+println("Standard Deviation for Maximum: ",STDDevMax)
+println("Minimum Average: ", avgMin)
+println("Standard Deviation for Minimum:",STDDevMin)
+println("Filtered Max element array: ",arrMax)
+println("Filtered Min element array: ",arrMin)
 
